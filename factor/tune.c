ed821c82b7eb656ee1135a2ad88722b9a1c8edc9
bbuhrow <bbuhrow@96aa8cfa-d74d-40c7-a1bb-ef7ebfd8c9c1>
2010-12-29 20:19:40 +0000

    + more robust method of reading last saved special-q + perform initial filtering run when restarting NFS factorizations

--- factor/tune.c
+++ factor/tune.c
@@ -13,6 +13,12 @@ void factor_tune(void)
 	char nfslist[6][200];
 	z n;
 	int i;
+	uint32 save_gbl_override_rel;
+	int save_gbl_override_blocks_flag;
+	struct timeval stop;	// stop time of this job
+	struct timeval start;	// start time of this job
+	TIME_DIFF *	difference;
+	double t_time;
 
 	zInit(&n);
 
@@ -43,13 +49,19 @@ void factor_tune(void)
 	//c90: 427351849650748515507228344120452096326780093349980867041485502247153375067354165128307841
 	//requires 4074867 relations
 	//c95: 48404068520546498995797968938385187958997290617596242601254422967869040251141325866025672337021
-	//requires relations
+	//requires 4783410 relations
 	//c100: 1802716097522165018257858828415111497060066282677325501816640492782221110851604465066510547671104729
 	//requires 4969315 relations
 	//c105: 466734409955806375058988820327650664396976790744285564594552020197119774272189758795312820988691316775181
 	//requires 5522597 relations
 	//c110: 48178889479314834847826896738914354061668125063983964035428538278448985505047157633738779051249185304620494013
 	//requires 5783845 relations
+
+	save_gbl_override_rel = gbl_override_rel;
+	save_gbl_override_blocks_flag = gbl_override_blocks_flag;
+
+	gbl_override_rel = 10000;		
+	gbl_override_blocks_flag = 1;
 
 	// for each of the siqs inputs
 	for (i=0; i<10; i++)
@@ -58,15 +70,26 @@ void factor_tune(void)
 		init_factobj(fobj);
 		zCopy(&fobj->fobj_factors[i].factor,&fobj->N);
 
-		//measure how long it takes to gather a fixed number of relations 
+		//measure how long it takes to gather a fixed number of relations 		
 		str2hexz(siqslist[i],&n);
+
+		gettimeofday(&start, NULL);
 		zCopy(&n,&fobj->qs_obj.n);
 		SIQS(fobj);
+		gettimeofday(&stop, NULL);
+		difference = my_difftime (&start, &stop);
+		t_time = ((double)difference->secs + (double)difference->usecs / 1000000);
+		free(difference);			
+		printf("elapsed time for ~10k relations of c%d = %6.4f seconds.\n",ndigits(&n),t_time);
+
 		clear_factor_list(fobj);
 
 		free_factobj(fobj);
 		free(fobj);
 	}
+
+	gbl_override_rel = save_gbl_override_rel;		
+	gbl_override_blocks_flag = save_gbl_override_blocks_flag;
 
 	zFree(&n);
 	return;
