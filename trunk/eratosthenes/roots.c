5dc289471bef1ae92a1ec4c93ea61e7727682280
bbuhrow <bbuhrow@96aa8cfa-d74d-40c7-a1bb-ef7ebfd8c9c1>
2011-02-22 05:16:56 +0000

    + progress with new bucket sieve in SOE.  all new stuff is protected with the INPLACE_BUCKET definition

--- trunk/eratosthenes/roots.c
+++ trunk/eratosthenes/roots.c
@@ -16,7 +16,7 @@ benefit from your work.
 
 void getRoots(soe_staticdata_t *sdata)
 {
-	int prime, prodN;
+	int prime, prodN, j;
 	uint64 startprime;
 	uint64 i;
 
@@ -43,6 +43,33 @@ void getRoots(soe_staticdata_t *sdata)
 #ifdef INPLACE_BUCKET
 	//for each bucket prime, compute its starting block,residue, and sieve location
 	//and add it to the appropriate linked list
+	j = 0;
+	for (i=BUCKETSTARTI; i<sdata->pboundi; i++, j++)
+	{
+		uint64 root;
+		uint32 p = sdata->sieve_p[i];
+		uint32 block, loc, residue;
+
+		sdata->bucket_primes[j].primeid = i;
+		root = (sdata->lowlimit / (uint64)p + 1) * (uint64)p;
+		block = (root - sdata->lowlimit) >> FLAGBITS;
+		loc = (root - sdata->lowlimit) & FLAGSIZEm1;
+		residue = root % sdata->prodN;
+		sdata->bucket_primes[j].loc = loc;
+		sdata->bucket_primes[j].res = p % prodN;
+
+		if (sdata->listptrs[block * sdata->prodN + residue] == NULL)
+		{
+			sdata->listptrs[block * sdata->prodN + residue] = &sdata->bucket_primes[j];
+			sdata->bucket_primes[j].next = (uint16)-1;
+		}
+		else
+		{
+			bucket_prime_t *lastp = sdata->listptrs[block * sdata->prodN + residue];
+			sdata->bucket_primes[j].next = i - lastp->primeid;
+			sdata->listptrs[block * sdata->prodN + residue] = &sdata->bucket_primes[j];
+		}
+	}
 
 #endif
 
