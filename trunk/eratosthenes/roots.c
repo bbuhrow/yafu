911facc5e7dd491b1a3f6967925ce6c94d2714fa
bbuhrow <bbuhrow@96aa8cfa-d74d-40c7-a1bb-ef7ebfd8c9c1>
2011-02-23 02:28:27 +0000

    + new SOE bucket sieve works (i.e. runs) but produces incorrect results as yet.  new technique can only be applied above a certain prime bound.

--- trunk/eratosthenes/roots.c
+++ trunk/eratosthenes/roots.c
@@ -16,7 +16,7 @@ benefit from your work.
 
 void getRoots(soe_staticdata_t *sdata)
 {
-	int prime, prodN, j;
+	int prime, prodN, j, max_dist = 0;
 	uint64 startprime;
 	uint64 i;
 
@@ -31,13 +31,12 @@ void getRoots(soe_staticdata_t *sdata)
 #ifdef INPLACE_BUCKET
 		if (i == BUCKETSTARTI)
 			break;
-#else
+#endif
 
 		inv = modinv_1(prodN,prime);
 		
 		sdata->root[i] = prime - inv;
 		sdata->lower_mod_prime[i] = (sdata->lowlimit + 1) % prime;
-#endif
 	}
 
 #ifdef INPLACE_BUCKET
@@ -48,28 +47,65 @@ void getRoots(soe_staticdata_t *sdata)
 	{
 		uint64 root;
 		uint32 p = sdata->sieve_p[i];
-		uint32 block, loc, residue;
+		uint32 block, loc, residue, steps;
 
 		sdata->bucket_primes[j].primeid = i;
+
+		//compute the location of the first hit on the number line 
+		//that is past the beginning of our interval
 		root = (sdata->lowlimit / (uint64)p + 1) * (uint64)p;
-		block = (root - sdata->lowlimit) >> FLAGBITS;
-		loc = (root - sdata->lowlimit) & FLAGSIZEm1;
+
+		//translate this to a hit on one of the residue lines within the interval
+		//begin by finding the residue class of the hit
 		residue = root % sdata->prodN;
-		sdata->bucket_primes[j].loc = loc;
+
+		//then find the number of steps on the residue line -- this is the bit offset
+		//of the hit in this residue line
+		steps = (root - sdata->lowlimit - residue) / sdata->prodN;
+						
+		//to prevent having to do the divisions every time we step this prime on the
+		//number line, record the number of steps and the residual step that this
+		//prime takes in residue space.  we need to know the residue class of the prime 
+		//itself also;
+		sdata->bucket_primes[j].steps = p / prodN + 1;
 		sdata->bucket_primes[j].res = p % prodN;
+
+		//now that we have all this info, iterate the first hit until it is on
+		//a residue line that we are sieving
+		while (!sdata->valid_residue[residue])
+		{
+			steps += sdata->bucket_primes[j].steps;
+			residue += sdata->bucket_primes[j].res;
+			if (residue >= sdata->prodN)
+			{
+				residue -= sdata->prodN;
+				steps++;
+			}
+		}			
+
+		//translate the final hit to block,offset notation
+		block = steps >> FLAGBITS;
+		sdata->bucket_primes[j].loc = steps & FLAGSIZEm1;
+
+		if (block >= sdata->blocks)
+			continue;
 
 		if (sdata->listptrs[block * sdata->prodN + residue] == NULL)
 		{
 			sdata->listptrs[block * sdata->prodN + residue] = &sdata->bucket_primes[j];
-			sdata->bucket_primes[j].next = (uint16)-1;
+			sdata->bucket_primes[j].next = (uint32)-1;
 		}
 		else
-		{
+		{			
 			bucket_prime_t *lastp = sdata->listptrs[block * sdata->prodN + residue];
 			sdata->bucket_primes[j].next = i - lastp->primeid;
+			if (i - lastp->primeid > max_dist)
+				max_dist = i - lastp->primeid;
 			sdata->listptrs[block * sdata->prodN + residue] = &sdata->bucket_primes[j];
 		}
 	}
+
+	printf("maximum distance in linked lists = %d\n",max_dist);
 
 #endif
 
