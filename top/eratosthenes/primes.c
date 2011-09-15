36ca1f1a03d5ed9e1928df1daf606216c26c8ad0
bbuhrow <bbuhrow@96aa8cfa-d74d-40c7-a1bb-ef7ebfd8c9c1>
2011-09-15 14:34:09 +0000

    + improvement to finding primes (faster and less memory)

--- top/eratosthenes/primes.c
+++ top/eratosthenes/primes.c
@@ -23,8 +23,7 @@ void primes_from_lineflags(thread_soedata_t *t)
 	uint8 *line = t->ddata.line;
 	uint64 current_line = t->current_line;
 	uint64 prime, num_alloc;
-	uint64 i,j,it;
-	//int past_low, k;
+	uint64 i,it;
 
 	it=0;
 
@@ -40,29 +39,6 @@ void primes_from_lineflags(thread_soedata_t *t)
 
 	//this will find all the primes in the line.  when other lines are appended, the primes
 	//will be out of order and will need to be sorted.
-	//past_low = 0;
-	//for (i=0;i<sdata->numlinebytes;i++)
-	//{
-	//	for (j = 0; j < 8; j++)
-	//	{
-	//		if (line[i] & nmasks[j])
-	//		{
-	//			prime = sdata->prodN * ((i << 3) + j) + 
-	//				sdata->rclass[current_line] + sdata->lowlimit;
-
-	//			//only store the prime if it is within our requested bounds
-	//			if ((prime >= sdata->orig_llimit))
-	//			{
-	//				past_low = 1;
-	//				ddata->primes[it] = prime;
-	//				it++;					
-	//			}
-	//		}
-	//	}
-	//	if (past_low)
-	//		break;
-	//}
-
 	for (i=0; i < sdata->numlinebytes; i++)
 	{
 		if (line[i] == 0)
@@ -172,6 +148,62 @@ void primes_from_lineflags(thread_soedata_t *t)
 	t->linecount = it;
 
 	return;
+}
+
+uint64 * merge_primes(uint64 *mergeprimes, uint64 *locprimes, 
+	uint64 num_p, uint64 linecount)
+{
+	//num_p is the size of mergeprimes
+	//linecount is the size of locprimes
+
+	//merge locprimes into mergeprimes.  
+	uint64 i1, i2, i3;
+	uint64 *new_merge;
+	
+	new_merge = (uint64 *)malloc((num_p + linecount) * sizeof(uint64));
+
+	i1 = i2 = i3 = 0;
+	while ((i1 < num_p) && (i2 < linecount)) {
+		if (mergeprimes[i1] < locprimes[i2])
+			new_merge[i3++] = mergeprimes[i1++];
+		else
+			new_merge[i3++] = locprimes[i2++];
+	}
+	while (i1 < num_p)
+		new_merge[i3++] = mergeprimes[i1++];
+
+	while (i2 < linecount)
+		new_merge[i3++] = locprimes[i2++];						
+
+	return new_merge;
+}
+
+uint64 * merge_primes32(uint64 *mergeprimes, uint32 *locprimes, 
+	uint64 num_p, uint64 linecount)
+{
+	//num_p is the size of mergeprimes
+	//linecount is the size of locprimes
+
+	//merge locprimes into mergeprimes.  
+	uint64 i1, i2, i3;
+	uint64 *new_merge;
+	
+	new_merge = (uint64 *)malloc((num_p + linecount) * sizeof(uint64));
+
+	i1 = i2 = i3 = 0;
+	while ((i1 < num_p) && (i2 < linecount)) {
+		if (mergeprimes[i1] < (uint64)locprimes[i2])
+			new_merge[i3++] = mergeprimes[i1++];
+		else
+			new_merge[i3++] = (uint64)locprimes[i2++];
+	}
+	while (i1 < num_p)
+		new_merge[i3++] = mergeprimes[i1++];
+
+	while (i2 < linecount)
+		new_merge[i3++] = (uint64)locprimes[i2++];						
+
+	return new_merge;
 }
 
 
